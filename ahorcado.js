4c8b575952c5de2f9d42075d4a93aa175b90c193
javobqcol <javobqcol@yahoo.es>
2022-02-16 00:16:10 -0500

    quinto commit

--- ahorcado.js
+++ ahorcado.js
@@ -33,17 +33,27 @@ function palabraAleatoria(lista){
 var botoniniciar = document.querySelector("#iniciar-juego");
 var juego = false;
 var listacaracteres = "";
-var contadorjuego = 0
-const maximointento = 7
+var contadorjuego = 0;
+const maximointento = 6;
 botoniniciar.addEventListener("click",function(event){
     event.preventDefault();
     palabra = palabraAleatoria(lista);
     console.log(palabra);
     juego=true;
+    window.scroll({
+        top: 700,
+        left: 100,
+        behavior: 'smooth'
+    });
+    ctx.beginPath();
     ctx.moveTo(70,700);
     ctx.lineTo(30,740);
     ctx.lineTo(110,740);
     ctx.lineTo(70,700);
+    ctx.lineTo(70, 400);
+    ctx.lineTo(200, 400);
+    ctx.lineTo(200, 440);
+
     ctx.strokeStyle = "#f00";
     ctx.stroke();
     arraycadena = Array.from(palabra);
@@ -63,10 +73,42 @@ botoniniciar.addEventListener("click",function(event){
     }
 });
 
-function pintarahorcado(numero){
-
-
+function pintarletras(){
+    console.log("entre");
+    ctx.fillStyle="blue";
+    for (var i = 0 ; i< arrayblanco.length;i++){
+        ctx.font="bold italic 25px arial";
+        ctx.fillText(arrayblanco[i], 130+i*40, 700);
+    }
+    
 }
+
+function pintarahorcado(numero){
+    console.log(numero);
+    if (numero == 1){
+        ctx.moveTo(220, 460);
+        ctx.arc(200, 460, 20,0, 2 * Math.PI);
+        ctx.moveTo(200, 460);
+    } else if (numero==2){
+        ctx.moveTo(200, 480);
+        ctx.lineTo(200, 580);
+    } else if (numero==3){
+        ctx.moveTo(200, 500);
+        ctx.lineTo(150, 450);
+    } else if (numero==4){
+        ctx.moveTo(200, 500);
+        ctx.lineTo(250, 450);
+    } else if (numero==5){
+        ctx.moveTo(200, 580);
+        ctx.lineTo(150, 650);
+    } else if (numero==6){
+        ctx.moveTo(200, 580);
+        ctx.lineTo(250, 650);
+    }
+    ctx.strokeStyle = "#f00";
+    ctx.stroke();
+}
+
 document.onkeypress = function(e) {
    if (juego){
         const expr = new RegExp("[^A-Za-z]", 'g')
@@ -86,18 +128,19 @@ document.onkeypress = function(e) {
             if (! acierto ){
                 console.log("ahorcado");
                 contadorjuego ++;
-                
+                pintarahorcado(contadorjuego);           
             }else{
                 console.log(arrayblanco);
-                
+                pintarletras(contador);
             }
             console.log(arraycadena.join(""));
             console.log(arrayblanco.join(""));
             if (arrayblanco.join("") == arraycadena.join("")){
-                console.log("ganaste se termino finito")
+                console.log("ganaste se termino finito");
             }
             if (contadorjuego >= maximointento){
-                console.log("te acabaste cavoevela")
+                console.log("te acabaste cavoevela");
+                juego=false;
             }
         }   
     }
